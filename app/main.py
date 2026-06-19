ff8a44ab43bd6e8a54fff9ab96ab6a716e2cbc99
Palina Mudrychenka <pmudrychenka@gmail.com>
2026-06-19 15:25:52 +0300

    the first jinja2 template added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi import Request
 
 import connect_db as db
+
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
@@ -11,14 +16,13 @@ app = FastAPI()
 
 We need it here to process CSS files which are connected to the main HTML file.
 '''
-app.mount("/static", StaticFiles(directory="frontend"), name="static")
+app.mount("/static", StaticFiles(directory="app/frontend"), name="static")
 
 @app.get("/login")
 def login_page():
-    return FileResponse("frontend/authorization.html")  # requesting the data from the source
+    return FileResponse("app/frontend/authorization.html")  # requesting the data from the source
 
 @app.post("/authorized")
-def login(username: str = Form(), password: str = Form()):
+def login(request: Request, username: str = Form(), password: str = Form()):
     db.add_user_data(username, password)
-    return {"username": username,
-            "password": password}
+    return templates.TemplateResponse(request, "authorized.html", {"username": username})

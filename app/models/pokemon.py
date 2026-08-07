646da10a171f991ca018fe5088342b8afadd0230
Palina Mudrychenka <pmudrychenka@gmail.com>
2026-08-08 00:54:28 +0300

    some quick fixes

--- app/models/pokemon.py
+++ app/models/pokemon.py
@@ -19,16 +19,6 @@ class Pokemon:
         all_genders = {0: 'Unknown', 1: 'W', 2: 'M', 3: 'W/M'}
         return all_genders[self.gender_id]
     
-    '''
-    @property
-    def types_div(self):
-        types_dict = dict()
-        
-        for el in self.types:
-            types_dict[TypeColor[el].name] = TypeColor[el].value
-        return types_dict
-    '''
-    
     @staticmethod
     def property_color(type):
         return TypeColor[type].value
